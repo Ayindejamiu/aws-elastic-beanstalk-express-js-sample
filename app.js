4feac7f07df570b1faef8758128557b877cd29d4
Ayinde Jamiu <ayindejamiu90@gmail.com>
2022-03-15 10:14:48 +0100

    my pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Mr. Ayinde Jamiu Kunle from my cloud series.'));
+app.get('/', (req, res) => res.send('Hello Mr. Ayinde Jamiu Kunle from pipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
